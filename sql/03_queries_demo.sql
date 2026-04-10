b3954f554c15adedc20c8bf0d78241ba69d43e03
mattos-rafael <rafa.rafa.rafa07@gmail.com>
2026-04-10 13:26:40 +0200

    queries labels fixed

--- sql/03_queries_demo.sql
+++ sql/03_queries_demo.sql
@@ -1,5 +1,5 @@
 -- Estudiantes aptos por vertical
-SELECT estudiante.nombre, COUNT(calificacion), MIN(vertical.vertical)
+SELECT estudiante.nombre, COUNT(calificacion) AS "aprobados", MIN(vertical.vertical) AS "vertical"
 FROM calificacion
 JOIN estudiante ON calificacion.estudiante_id = estudiante.id
 JOIN promocion ON estudiante.promocion_id = promocion.id
@@ -9,7 +9,7 @@ GROUP BY estudiante.nombre
 ORDER BY MIN(vertical.vertical)
 
 -- Estudiantes por promocion
-SELECT e.nombre, COUNT(c.resultado), MIN(p.promocion), MIN(e.fecha_comienzo)
+SELECT e.nombre, COUNT(c.resultado) AS "aprobados", MIN(p.promocion) AS "promocion", MIN(e.fecha_comienzo) AS "fecha_comienzo"
 FROM calificacion AS c
 LEFT JOIN estudiante AS e ON c.estudiante_id = e.id
 LEFT JOIN promocion AS p ON e.promocion_id = p.id
@@ -18,7 +18,7 @@ GROUP BY e.nombre
 ORDER BY MIN(p.promocion)
 
 -- 5 Proyectos con mas aptos
-SELECT t.tipo_proyecto, COUNT(c.resultado)
+SELECT t.tipo_proyecto, COUNT(c.resultado) AS "aprobados"
 FROM calificacion AS c
 JOIN tipo_proyecto AS t ON c.tipo_proyecto_id = t.tipo_proyecto_id
 WHERE c.resultado = 'Apto'
@@ -27,14 +27,14 @@ ORDER BY COUNT(c.resultado) DESC
 LIMIT 5
 
 -- Numeros de estudiantes aptos por proyecto
-SELECT t.tipo_proyecto, COUNT(c.resultado)
+SELECT t.tipo_proyecto, COUNT(c.resultado) AS "aprobados"
 FROM calificacion AS c
 JOIN tipo_proyecto AS t ON c.tipo_proyecto_id = t.tipo_proyecto_id
 WHERE c.resultado = 'Apto'
 GROUP BY t.tipo_proyecto
 
 -- 5 Proyectos con mas aptos
-SELECT t.tipo_proyecto, COUNT(c.resultado)
+SELECT t.tipo_proyecto, COUNT(c.resultado) AS "suspensos"
 FROM calificacion AS c
 JOIN tipo_proyecto AS t ON c.tipo_proyecto_id = t.tipo_proyecto_id
 WHERE c.resultado = 'No Apto'
@@ -43,21 +43,21 @@ ORDER BY COUNT(c.resultado) DESC
 LIMIT 5
 
 -- Numero de estudiantes 'No Aptos' por proyecto
-SELECT t.tipo_proyecto, COUNT(c.resultado)
+SELECT t.tipo_proyecto, COUNT(c.resultado) AS "suspensos"
 FROM calificacion AS c
 JOIN tipo_proyecto AS t ON c.tipo_proyecto_id = t.tipo_proyecto_id
 WHERE c.resultado = 'No Apto'
 GROUP BY t.tipo_proyecto
 
 -- Numero de estudiantes por vertical
-SELECT v.vertical, COUNT(e)
+SELECT v.vertical, COUNT(e) AS "estudiantes"
 FROM estudiante AS e
 LEFT JOIN promocion AS p ON e.promocion_id = p.id
 LEFT JOIN vertical AS v ON p.vertical_id = v.id
 GROUP BY v.vertical
 
 -- Numero de estudiantes por campus
-SELECT c.nombre, COUNT(e)
+SELECT c.nombre, COUNT(e) AS "num estudiantes"
 FROM estudiante AS e
 LEFT JOIN campus AS c ON e.promocion_id = c.id
 GROUP BY c.nombre
